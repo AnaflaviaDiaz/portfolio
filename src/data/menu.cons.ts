762c43d5a86374d34d8e7250dbc6233bf3143406
Anaflavia <anaflaviadmar@gmail.com>
2022-06-05 21:53:29 +0200

    feat(toolbar): add sidebar when device is mobile

---
+++ src/data/menu.cons.ts
@@ -0,0 +1,9 @@
+import { Menu } from "../models/menu.interface";
+
+export const MENUS: Menu[] = [
+  { href: '#portfolio', name: 'Portfolio'},
+  { href: '#about-me', name: 'About me'},
+  { href: '#tech-know', name: 'Tech know'},
+  { href: '#skills', name: 'Skills'},
+  { href: '#contact-me', name: 'Contact me'},
+];
